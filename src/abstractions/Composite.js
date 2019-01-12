5fd4a2af296d3acc21593bdf08cd43ec450614a6
Derk Norton <derk.norton@gmail.com>
2019-01-12 12:18:16 -0700

    Removed 'value instanceof Component' check from Composite.asComponent() method once and for all since it can't made to behave with the circular dependency.

--- src/abstractions/Composite.js
+++ src/abstractions/Composite.js
@@ -88,12 +88,8 @@ Composite.asComponent = function(value) {
             component = new elements.Number(value);
             break;
         default:
-            if (value instanceof Component) {
-                // leave it since it is already a component
-                component = value;
-            } else {
-                throw new Error('BUG: Only primitive JS types (strings, numbers, and booleans) can be converted to components: ' + value);
-        }
+            // leave it since it is already a component
+            component = value;
     }
     return component;
 };
