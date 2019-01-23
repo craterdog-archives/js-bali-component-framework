7eb7e7fd02fe663b291ec0436f18662c9a7c4072
Derk Norton <derk.norton@gmail.com>
2019-01-23 09:41:05 -0700

    Added shortcuts to the master index.js file.

--- index.js
+++ index.js
@@ -82,3 +82,38 @@ exports.Stack = collections.Stack;
 // BALI PARSER
 exports.Parser = require('./src/utilities/Parser').Parser;  // depends on EVERYTHING
 exports.parser = new exports.Parser(false);  // depends on Parser
+
+
+// SHORT CUTS
+exports.format = function(component) {
+    return exports.formatter.formatComponent(component);
+};
+exports.parse = function(document) {
+    return exports.parser.parseDocument(document);
+};
+
+
+// CONSTANTS
+exports.NONE = exports.Pattern.fromLiteral('none');
+exports.ANY = exports.Pattern.fromLiteral('any');
+exports.FALSE = exports.Probability.fromLiteral('false');
+exports.TRUE = exports.Probability.fromLiteral('true');
+exports.PI = exports.Angle.fromLiteral('~pi');
+exports.E = exports.Number.fromLiteral('e');
+exports.PHI = exports.Number.fromLiteral('phi');
+exports.ZERO = exports.Number.fromLiteral('0');
+exports.ONE = exports.Number.fromLiteral('1');
+exports.I = exports.Number.fromLiteral('1i');
+exports.UNDEFINED = exports.Number.fromLiteral('undefined');
+exports.INFINITY = exports.Number.fromLiteral('infinity');
+
+
+// PARAMETERS
+exports.degrees = new exports.Parameters(exports.parse('[$units: $degrees]'));
+exports.radians = new exports.Parameters(exports.parse('[$units: $radians]'));
+exports.polar = new exports.Parameters(exports.parse('[$format: $polar]'));
+exports.rectangular = new exports.Parameters(exports.parse('[$format: $rectangular]'));
+exports.base2 = new exports.Parameters(exports.parse('[$base: 2]'));
+exports.base16 = new exports.Parameters(exports.parse('[$base: 16]'));
+exports.base32 = new exports.Parameters(exports.parse('[$base: 32]'));
+exports.base64 = new exports.Parameters(exports.parse('[$base: 64]'));
