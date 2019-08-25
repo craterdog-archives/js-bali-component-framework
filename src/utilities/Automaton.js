bb48f5263981a9042dcae68ef0faa9fbab331120
Derk Norton <derk.norton@gmail.com>
2019-08-25 17:58:24 -0600

    Added unit tests for the Automaton class.

--- src/utilities/Automaton.js
+++ src/utilities/Automaton.js
@@ -23,26 +23,46 @@ const utilities = require('../utilities');
  * This constructor creates a new finite state automaton using the specified event type
  * array and state transition object.
  * <pre>
- * eventTypes:  [  $event1,   $event2, ...   $eventM]
+ * eventTypes:  ['$event1', '$event2', ... '$eventM']
  * nextStates: {
- *     $state1: [undefined,   $state2, ... undefined]
- *     $state2: [  $state3,   $stateN, ...   $state1]
+ *     $state1: [undefined, '$state2', ... undefined]
+ *     $state2: ['$state3', '$stateN', ... '$state1']
  *                         ...
- *     $stateN: [  $state1, undefined, ...   $state3]
+ *     $stateN: ['$state1', undefined, ... '$state3']
  * }
  * </pre>
  * The first state in the nextStates object is the initial state of the finite state automaton.
  * 
- * @param {Array} eventTypes An array of the possible event types.
- * @param {Object} nextStates An object defining the possible states and allowed transitions
- * between them given specific event types.
+ * @param {Array} eventTypes An array of the possible event types as strings.
+ * @param {Object} nextStates An object defining the possible states as strings and allowed
+ * transitions between them given specific event types.
  * @returns {Automaton} A new finite state automaton.
  */
 function Automaton(eventTypes, nextStates) {
     var currentState;
     const numberOfEventTypes = eventTypes.length;
+    eventTypes.forEach(function(event) {
+        if (typeof event !== 'string') {
+            throw new utilities.Exception({
+                $module: '/bali/utilities/Automaton',
+                $procedure: '$Automaton',
+                $exception: '$invalidType',
+                $event: '"' + event + '"',
+                $text: '"Each event must be of type string."'
+            });
+        }
+    });
     var numberOfStates = 0;
     for (const state in nextStates) {
+        if (typeof state !== 'string') {
+            throw new utilities.Exception({
+                $module: '/bali/utilities/Automaton',
+                $procedure: '$Automaton',
+                $exception: '$invalidType',
+                $state: '"' + state + '"',
+                $text: '"Each state must be of type string."'
+            });
+        }
         currentState = currentState || state;
         if (nextStates[state].length !== numberOfEventTypes) {
             throw new utilities.Exception({
@@ -54,6 +74,18 @@ function Automaton(eventTypes, nextStates) {
                 $text: '"Each next state list must have the same length as the number of event types."'
             });
         }
+        nextStates[state].forEach(function(transition) {
+            if (transition && Object.keys(nextStates).indexOf(transition) < 0) {
+                throw new utilities.Exception({
+                    $module: '/bali/utilities/Automaton',
+                    $procedure: '$Automaton',
+                    $exception: '$invalidParameter',
+                    $expected: Object.keys(nextStates),
+                    $actual: transition,
+                    $text: '"A next state was found that is not in the possible states."'
+                });
+            }
+        });
         numberOfStates++;
     }
 
@@ -69,14 +101,24 @@ function Automaton(eventTypes, nextStates) {
                 $procedure: '$validateEvent',
                 $exception: '$invalidEvent',
                 $event: event,
-                $expected: nextStates[state],
-                $text: '"Each next state list must have the same length as the number of event types."'
+                $state: currentState,
+                $text: '"The event is not allowed in the current state."'
             });
         }
     };
 
     this.transitionState = function(event) {
         const index = eventTypes.indexOf(event);
+        if (!nextStates[currentState][index]) {
+            throw new utilities.Exception({
+                $module: '/bali/utilities/Automaton',
+                $procedure: '$transitionState',
+                $exception: '$invalidEvent',
+                $event: event,
+                $state: currentState,
+                $text: '"The event is not allowed in the current state."'
+            });
+        }
         currentState = nextStates[currentState][index];
     };
 
---
+++ test/TestAutomaton.js
@@ -0,0 +1,91 @@
+/************************************************************************
+ * Copyright (c) Crater Dog Technologies(TM).  All Rights Reserved.     *
+ ************************************************************************
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.        *
+ *                                                                      *
+ * This code is free software; you can redistribute it and/or modify it *
+ * under the terms of The MIT License (MIT), as published by the Open   *
+ * Source Initiative. (See http://opensource.org/licenses/MIT)          *
+ ************************************************************************/
+
+const mocha = require('mocha');
+const expect = require('chai').expect;
+const bali = require('../');
+
+
+describe('Bali Component Framework™', function() {
+
+    describe('Test the automaton with invalid parameters.', function() {
+
+        it('should detect an empty event types array', function() {
+            expect(
+                function() {
+                    bali.automaton([]);
+                }
+            ).to.throw();
+        });
+
+        it('should detect an empty state transitions array', function() {
+            expect(
+                function() {
+                    bali.automaton(['$event1', '$event2'], {
+                        $state1: [],
+                        $state2: []
+                    });
+                }
+            ).to.throw();
+        });
+
+        it('should detect an invalid state in the transitions array', function() {
+            expect(
+                function() {
+                    bali.automaton([ '$event1', '$event2' ], {
+                        $state1: ['$state2', undefined],
+                        $state2: [undefined, '$state3']
+                    });
+                }
+            ).to.throw();
+        });
+
+    });
+
+    describe('Test the automaton with valid parameters.', function() {
+
+        it('should run a new automaton correctly', function() {
+            const fsa = bali.automaton([ '$event1', '$event2' ], {
+                $state1: ['$state2', undefined],
+                $state2: [undefined, '$state1']
+            });
+            expect(fsa.getState()).to.equal('$state1');
+            fsa.validateEvent('$event1');
+            fsa.transitionState('$event1');
+            expect(fsa.getState()).to.equal('$state2');
+            fsa.validateEvent('$event2');
+            fsa.transitionState('$event2');
+            expect(fsa.getState()).to.equal('$state1');
+        });
+
+        it('should detect invalid state transitions', function() {
+            expect(
+                function() {
+                    bali.automaton([ '$event1', '$event2' ], {
+                        $state1: ['$state2', undefined],
+                        $state2: [undefined, '$state1']
+                    });
+                    fsa.validateEvent('$event2');
+                }
+            ).to.throw();
+            expect(
+                function() {
+                    bali.automaton([ '$event1', '$event2' ], {
+                        $state1: ['$state2', undefined],
+                        $state2: [undefined, '$state1']
+                    });
+                    fsa.transitionState('$event2');
+                }
+            ).to.throw();
+        });
+
+    });
+
+});
