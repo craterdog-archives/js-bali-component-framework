bc8fec9207f3a4b4290e2c9ca324f5af49bf5d96
Derk Norton <derk.norton@gmail.com>
2020-03-27 16:01:18 -0600

    Fixed comment in Gruntfile.js.

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
       }
     },
 
-    // grunt--eslint plugin configuration (lint for JS)
+    // grunt-eslint plugin configuration (lint for JS)
     eslint: {
       options: {
       },
